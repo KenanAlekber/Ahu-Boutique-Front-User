1372e92b0022a75936d0c04dae413f5ffab32d32
Kənan Ələkbərov <kananha@code.edu.az>
2023-12-05 16:51:24 +0400

    Create new project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/Routes";
+
+const queryClient = new QueryClient();
+
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ChakraProvider>
+                    <AppRoutes />
+                </ChakraProvider>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
+}
+
+export default App;
---
+++ src/routes/Routes.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../app/main/Home'
+import { ROUTES } from './const'
+import Contact from '../app/components/Contact/Contact'
+import ProductDetail from '../app/components/Home/ProductDetail/ProductDetail'
+import { Products } from '../app/components/Shop/Products/Products'
+import Login from '../app/auth/Login/Login'
+import Register from '../app/auth/Register/Register'
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path={ROUTES.MAIN.HOME} element={<Home />} />
+            <Route path={ROUTES.CONTACT} element={<Contact />} />
+            <Route path={ROUTES.PRODUCT.DETAIL} element={<ProductDetail />} />
+            <Route path={ROUTES.SHOP} element={<Products />} />
+            <Route path={ROUTES.USER.LOGIN} element={<Login />} />
+            <Route path={ROUTES.USER.REGISTER} element={<Register />} />
+            {/*
+            <Route path={ROUTES.USER.FORGOTPASSWORD} element={<ForgotPassword />} />
+            <Route path={ROUTES.USER.RESETPASSWORD} element={<ResetPassword />} /> 
+            */}
+        </Routes>
+    )
+}
+
+export default AppRoutes
